8e385002d207253eda14b58d737027614ab35ddd
Guru <guruvsb@gmail.com>
2026-08-29 00:52:22 +0530

    Auto-create superuser on deploy for free-tier Render

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'Guru@2720')
+"
